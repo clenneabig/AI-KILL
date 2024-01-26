2c663e15d0fc28e08d13039e624ed3055821167a
ErinT-R <erin.thomasriley@gmail.com>
2024-01-27 11:14:57 +1300

    added acceleration to bullets
    please don't look at my code its not good

--- BulletPattens/bullet.gd
+++ BulletPattens/bullet.gd
@@ -3,12 +3,19 @@ extends Node
 
 var speed = Vector2(0, 0)
 var dur = 20
+var curret_acc =  Vector2(0, 0)
+var acc = 1.0
+var accel = false
+var deccel = false
+var Acc_Cap = 100.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	self.position += speed.rotated(self.rotation) * delta
+	self.position += (speed.rotated(self.rotation) * delta) + curret_acc
+	if(accel and curret_acc.x>(-Acc_Cap)): curret_acc.x -= acc
+	if(deccel and curret_acc.x < 8): curret_acc.x += acc/10
 	dur -= delta
-	if dur <= 0:
+	if dur <= 0: #avoid memory eating
 		queue_free()
 	
 
--- BulletPattens/emit_deccelerated.gd
+++ BulletPattens/emit_deccelerated.gd
@@ -1,19 +1,15 @@
-extends Emitter
-class_name Straight_Shot
+extends Straight_Shot
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	shoot_timer.wait_time = 1.0
+	vel = 300
 	super()
-	spawn_count = 1
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 
 func _shoot(vel, _i):
 	var nb = bullet_scene.instantiate()
+	nb.deccel = true
 	nb.speed = Vector2(-vel, 0)
 	nb.position = position
 	nb.rotation = rotation
