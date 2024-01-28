d39ced59195784a4445cd44f4256d36caaaf682a
Abigail Clennell <abigailaclennell@gmail.com>
2024-01-28 16:09:52 +1300

    menu

--- main_menu.gd
+++ main_menu.gd
@@ -1,7 +1,13 @@
 extends Control
 
 
-
-
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://Levels/level_1.tscn")
+	get_tree().change_scene_to_file("res://Cutscene.tscn")
+
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+
+func _on_credits_pressed():
+	get_tree().change_scene_to_file("res://credits.tscn")
