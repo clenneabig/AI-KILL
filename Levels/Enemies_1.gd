ae185b0bc60e8c3b74fdf2fe82585e2d9b0fde7c
ErinT-R <erin.thomasriley@gmail.com>
2024-01-28 13:11:54 +1300

    level_1 complete!!!!

--- Levels/Enemies_1.gd
+++ Levels/Enemies_1.gd
@@ -6,7 +6,9 @@ extends Node2D
 @onready var group4 = $Group_4
 @onready var MidBoss = $MidHolder/MidBoss
 @onready var MidHold = $MidHolder
-
+@onready var group5 = $Group_5
+@onready var group6 = $Group_6
+@onready var BossHold = $BossHolder
 
 var g_move_speed = 150.0
 var paused = false
@@ -22,7 +24,7 @@ enum Groups{
 	Group6,
 	Boss
 }
-var CurrentGroup = Groups.Mid_Boss
+var CurrentGroup = Groups.Group5
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -68,7 +70,7 @@ func _process(delta):
 			if(group2.get_child_count() == 0):
 				CurrentGroup = Groups.Group3
 		Groups.Group3:
-			var childrenNo = group3.get_child_count()-1
+			var childrenNo = group3.get_child_count()
 			for path in group3.get_children():
 				path.get_child(0).progress += g_move_speed*2 * delta
 				if(path.get_child(0).progress_ratio >= 1.0):
@@ -97,4 +99,21 @@ func _process(delta):
 				t.queue_free()
 				CurrentGroup = Groups.Group5
 		Groups.Group5:
-			pass
+			var childrenNo = group5.get_child_count()
+			for path in group5.get_children():
+				path.get_child(0).progress += g_move_speed*2 * delta
+				if(path.get_child(0).get_child_count() <= 0):
+					childrenNo-=1
+			if(childrenNo <= 0):
+				CurrentGroup = Groups.Group6
+		Groups.Group6:
+			var childrenNo = group6.get_child_count()
+			for path in group6.get_children():
+				path.get_child(0).progress += g_move_speed*2 * delta
+				if(path.get_child(0).get_child_count() <= 0):
+					childrenNo-=1
+			if(childrenNo <= 0):
+				CurrentGroup = Groups.Boss
+		Groups.Boss:
+			if(BossHold.get_child_count() != 0):
+				BossHold.get_child(0).move_to_centre()
